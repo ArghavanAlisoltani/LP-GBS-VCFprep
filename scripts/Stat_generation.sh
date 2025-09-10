953babb2b8987c0d95991d3bafe487b08c7fbb85
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-09-09 20:34:22 -0500

    Update Stat_generation.sh

--- scripts/Stat_generation.sh
+++ scripts/Stat_generation.sh
@@ -1,17 +1,27 @@
 #---------------------------------------------------------------------------------------#
-#S1
-#CSI indexes (not TBI)
-#module load bcftools/1.8
+#0. CSI indexes (not TBI)
+module load bcftools/1.8
 bcftools index -c All_1a1b_renamed.vcf.gz
 #or
 #samtools index -c alignments.bam (BAM).
-#---------------------------------------------------------------------------------------#
-#S2
-#Missingness per individual
-module load vcftools/0.1.16
-vcftools --gzvcf All_1a1b_renamed.vcf.gz --missing-indv
 
 #---------------------------------------------------------------------------------------#
-#S3
-# Site-level_snapshot
-bcftools query -f '%CHROM\t%POS\t%QUAL\t%INFO/DP\t%INFO/NS\t%INFO/MQ\t%INFO/AN\t%INFO/AC\t%INFO/AF\n' All_1a1b_renamed.vcf.gz
+#1. Missingness per individual
+module load vcftools/0.1.16
+vcftools --gzvcf ../All_1a1b_renamed.vcf.gz --missing-indv
+
+#---------------------------------------------------------------------------------------#
+#2. Depth per individual
+vcftools --gzvcf ../All_1a1b_renamed.vcf.gz --depth
+
+#---------------------------------------------------------------------------------------#
+#5. Heterozygosity outliers (paralogs/contamination)
+
+#---------------------------------------------------------------------------------------#
+#3. Site-level_snapshot
+bcftools query -f '%CHROM\t%POS\t%QUAL\t%INFO/DP\t%INFO/NS\t%INFO/MQ\t%INFO/AN\t%INFO/AC\t%INFO/AF\n' ../All_1a1b_renamed.vcf.gz
+
+#---------------------------------------------------------------------------------------#
+bcftools query -f '%CHROM\t%POS[\t%DP]\n' file.vcf.gz
+
+
