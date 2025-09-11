7797522bd2707affc9422d9b1d9aec55f62ea3e7
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-09-11 12:41:43 -0500

    Update S9d_renaming_samples.sh

--- scripts/S9d_renaming_samples.sh
+++ scripts/S9d_renaming_samples.sh
@@ -1,1 +1,10 @@
-
+awk 'BEGIN{OFS="\t"}
+  /^##/ { print; next }                                   # pass meta
+  /^#CHROM/ {                                             # rename samples
+    for (i=10; i<=NF; i++) {
+      n=split($i,a,"_"); $i=a[n]
+    }
+    print; next
+  }
+  { print }                                               # body
+' All_taxa_merged_sorted_tworules.vcf > renamed_samples_1489_merged_sorted_tworules.vcf
