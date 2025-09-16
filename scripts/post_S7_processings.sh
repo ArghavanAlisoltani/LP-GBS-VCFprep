31b28cfdb89205e04fa8243a047debaf210606e8
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2025-09-15 21:43:15 -0500

    Update post_S7_processings.sh

--- scripts/post_S7_processings.sh
+++ scripts/post_S7_processings.sh
@@ -4,12 +4,12 @@
 #---------------------------------------------------------------------------------------#
 #0. Compress to bgzip'd VCF
 module load bcftools/1.8
-bcftools view -Oz -o renamed_samples_1489_merged_sorted_tworules.vcf.gz \
-  renamed_samples_1489_merged_sorted_tworules.vcf
+bcftools view -Oz -o renamed_1ab_samples_1489_merged_sorted_tworules.vcf.gz \
+  renamed_1ab_samples_1489_merged_sorted_tworules.vcf
 
 # (optional but smart) ensure position-sorted before indexing
-bcftools sort -Oz -o renamed_samples_1489_merged_sorted_tworules.sorted.vcf.gz \
-  renamed_samples_1489_merged_sorted_tworules.vcf.gz
+bcftools sort -Oz -o renamed_1ab_samples_1489_merged_sorted_tworules.vcf.gz \
+  renamed_1ab_samples_1489_merged_sorted_tworules.vcf
 
 # 1) CSI indexes (not TBI)
 module load bcftools/1.8
